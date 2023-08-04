515033a7a3aefaeb554611c791bd440e2fc0ea62
vitor.gama <vitorggam@hotmail.com>
2023-08-04 10:30:12 -0300

    Calculo media

--- exercicio-5.php
+++ exercicio-5.php
@@ -4,27 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 5</title>
+
+    <style>
+       .reprovado { color: red;}
+       .aprovado { color: blue;}
+    </style>
 </head>
 <body>
-    <h1>Exercicio 5 -> Funções</h1>
+    <h1>Exercicio 5 - Calcular</h1>
 
     <hr>
 
     <?php
-    function calcularMedia( $nota1, $nota2 ){
+    function calcularMedia(float $nota1, float $nota2 ):float {
         $total = ($nota1 + $nota2) / 2; 
         return $total;
     }   
 
-    function situacaoAluno(int $total): string {
+    function situacaoAluno(float $total): string {
         if ($total < 7){
-            return "Aluno reprovado";
+            return "reprovado";
         }
-        return "Aluno aprovado";
+        return "aprovado";
     }    
+
+    $media = calcularMedia(8, 10);
      ?>
-    <p>Média: <?=calcularMedia(8, 7)?></p>
-     <p>Situação: <?=situacaoAluno(8, 7)?></p>
+
+    <p>Média: <?=$media?></p>
+     <p>Situação: <span class="<?=situacaoAluno($media)?>"> <?=situacaoAluno($media)?></span></p>
 
      
 
