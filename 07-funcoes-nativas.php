ad6dfeb70f95884fe5d3eb393ddf4c8457ac5990
vitor.gama <vitorggam@hotmail.com>
2023-08-07 09:37:45 -0300

    senhas

--- 07-funcoes-nativas.php
+++ 07-funcoes-nativas.php
@@ -144,7 +144,7 @@
         document.body.innerHTML = '<h1>Sou ráqui!! mwhaha : ( </h1>'
         </script>";
 
-        echo $ataque;
+       // echo $ataque;
 
         //Execução sem sanitização (script é válido)
         //echo ataque;
@@ -153,16 +153,48 @@
 
         //Execução com sanitização (script é anulado)
         echo $ataqueSanitizado;
-
-
     ?>
 
+    <hr>   
+    
+    <h2>Segurança (Criptografia de dados)</h2>
+    <?php
+    //Senha em texto puro (plain text)
+    $senha = "123senac";
 
-    <hr>
+    /* Algoritmos mais comuns para criptografia:
+    MD5, SHA1, <SHA256></SHA256>
     
-    
-    <h2>Segurança</h2>
+    Estes algoritmos pegan os dados e os codificam/criptografam 
+    tornando um "hash" de dados embaralhados.    */
+    $senhaMD5 = md5($senha);
+$senhaSHA1 = sha1($senha);
+$senhaSHA256 = hash('sha256', $senha);
+?>
+<p>Senha (texto puro): <?=$senha?></p>
+<p>Senha (MD5): <?=$senhaMD5?></p>
+<p>Senha (SHA1): <?=$senhaSHA1?></p>
+<p>Senha (SHA256): <?=$senhaSHA256?></p>
 
-    
+<?php
+/* Método recomendado para segurança de senhas
+no PHP. */
+$senhaSegura = password_hash($senha, PASSWORD_DEFAULT);
+?>
+<p>Senha (com password_hash): <?=$senhaSegura?></p>
+
+<?php
+/* Como verificar o hash da senha se ele pode mudar? */
+$senhaDigitada = "123senacc";
+
+// if ( $senhaDigitada === $senhaSegura ) { // NÃO DÁ CERTO!
+if( password_verify($senhaDigitada, $senhaSegura) ){    
+    echo "senha correta, pode entrar...";
+} else {
+    echo "senha errada, vaza daqui disgraça!";
+}
+
+?>
+
 </body>
 </html>
