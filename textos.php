4d103cd17514305759e4e1b60fd168e0a8b99e4f
vitor.gama <vitorggam@hotmail.com>
2023-08-07 10:52:06 -0300

    require e include

---
+++ textos.php
@@ -0,0 +1,13 @@
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil eos perspiciatis cum fugit obcaecati soluta? Sed, quasi numquam atque, optio natus accusamus suscipit, consequuntur commodi inventore reprehenderit impedit quos similique. Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde optio aspernatur expedita soluta culpa enim nemo nihil eaque molestiae ad dolore, aperiam, saepe maiores incidunt perferendis ipsum placeat odit dolorem.
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo quas voluptatem sint voluptas ex beatae facilis laborum repellendus delectus pariatur, officiis corporis veniam fugiat? Esse, illo explicabo. Enim, expedita quas!
+</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil eos perspiciatis cum fugit obcaecati soluta? Sed, quasi numquam atque, optio natus accusamus suscipit, consequuntur commodi inventore reprehenderit impedit quos similique. Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde optio aspernatur expedita soluta culpa enim nemo nihil eaque molestiae ad dolore, aperiam, saepe maiores incidunt perferendis ipsum placeat odit dolorem.
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo quas voluptatem sint voluptas ex beatae facilis laborum repellendus delectus pariatur, officiis corporis veniam fugiat? Esse, illo explicabo. Enim, expedita quas!
+</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil eos perspiciatis cum fugit obcaecati soluta? Sed, quasi numquam atque, optio natus accusamus suscipit, consequuntur commodi inventore reprehenderit impedit quos similique. Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde optio aspernatur expedita soluta culpa enim nemo nihil eaque molestiae ad dolore, aperiam, saepe maiores incidunt perferendis ipsum placeat odit dolorem.
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo quas voluptatem sint voluptas ex beatae facilis laborum repellendus delectus pariatur, officiis corporis veniam fugiat? Esse, illo explicabo. Enim, expedita quas!
+</p>
+
+<?php include "lista.html"?>
+
+
