10cc2f110efe135de333f15d677ae1c5f8a3a41a
vitor.gama <vitorggam@hotmail.com>
2023-08-08 12:02:15 -0300

    aplicando isset

--- processa-post.php
+++ processa-post.php
@@ -12,35 +12,40 @@
 
     <p>Receber e processar dados via <b>POST</b></p>
 
+    <?php
+    if (empty($_POST["nome"]) || empty($_POST["email"])) {
+        // Campos nome e/ou email estão vazios
+    ?>
+        
+    <p>Preencha nome e e-mail</p>
+    <p><a href="10-formulario.html">Voltar</a></p>
 
     <?php
-    /*Se os campos nome e email estão vazios*/ 
-    if( empty ($_POST["nome"]) || empty($_POST["email"]) ){?>
-        <p>preencha nome e e-mail</p>
-        <p><a href="10-formulario.html"> Voltar</a></p>
-        <?php
     } else {
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $mensagem = $_POST["mensagem"];
+        $idade = isset($_POST["idade"]) ? $_POST["idade"] : "";
+        $interesses = isset($_POST["interesses"]) ? $_POST["interesses"] : array();
 
-
-    $nome = $_POST["nome"];
-    $email = $_POST["email"];
-    $mensagem = $_POST["mensagem"];
+        // Mostrar os dados somente se a mensagem não estiver vazia
+        $mostrarMensagem = !empty($mensagem);
     ?>
 
     <h2>Dados:</h2>
     <ul>
-        <li>Nome: </li>
-        <li>E-mail: </li>
-        <li>Mensagem: </li>
-        
+        <li>Nome: <?= $nome ?></li>
+        <li>E-mail: <?= $email ?></li>
+        <li>Idade: <?= $idade ?></li>
+        <li>Interesses: <?= implode(", ", $interesses) ?></li>
+
+        <?php if ($mostrarMensagem) { ?>
+            <li>Mensagem: <?= $mensagem ?></li>
+        <?php } ?>
     </ul>
+
     <?php
     }
     ?>
-
-
-
-
-    
 </body>
 </html>
